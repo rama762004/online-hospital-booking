7e4c3bb5e7a82c744ab5e81770e07383e48cd816
rama762004 <149177495+rama762004@users.noreply.github.com>
2023-10-27 20:29:30 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+document.getElementById('toggleTheme').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+});
+// You can add JavaScript functionality here if needed
+document.getElementById("appointment-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // You can add code here to handle the form submission, like sending the data to a server.
+});
+// You can add JavaScript functionality here if needed
+// You can add JavaScript functionality here if needed
